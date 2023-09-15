32ad1ab44c854b73fee2243554b9ed2c7e03320a
mengwei <mengwei@example.com>
2023-09-15 15:56:01 -0700

    first commit

---
+++ react-todo-list/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* all we have is the App */}
+    <App />
+  </React.StrictMode>,
+)
