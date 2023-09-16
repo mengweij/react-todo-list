5bdc079df65ba394ba1c81651787b82406d935af
mengwei <mengwei@example.com>
2023-09-15 17:12:45 -0700

    update display of todos

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -36,20 +36,17 @@ export default function App() {
       
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          )
+        })}
       </ul>
     </>
   )
